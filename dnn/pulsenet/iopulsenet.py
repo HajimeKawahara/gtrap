c456f4c5e06e0a7cec61f30ca3fed3e45221037b
Hajime Kawahara <divrot@gmail.com>
2019-09-03 08:43:30 +0900

    reorganize directories

--- dnn/pulsenet/iopulsenet.py
+++ dnn/pulsenet/iopulsenet.py
@@ -1,6 +1,41 @@
 import numpy as np
 
 
+def makearrCA(flist,default_pick=0):
+    #LC and local BIP and local AST
+    #,[lab],lcs,lcsw,asinds,asindsw,infogap,infogapw,starinfo
+    lab=[]
+    X=[]
+    Xw=[]
+    B=[]
+    A=[]
+
+    info=[]
+    for fn in flist:
+        d=np.load(fn,allow_pickle=True)
+        if d["arr_0"][0]==-1:
+            labtmp=default_pick
+        else:
+            labtmp=d["arr_0"][0]
+        lab.append(labtmp)
+        X.append(np.array([d["arr_1"]+1.0]).T)
+        Xw.append(np.array([d["arr_2"]+1.0]).T)
+        B.append(np.array([d["arr_7"]+1.0,d["arr_9"]+1.0]).T)
+        A.append(np.array([d["arr_4"]+1.0]).T)
+        info.append(d["arr_11"])
+    lab=np.array(lab).astype(np.int32)
+    X=np.array(X)
+    Xw=np.array(Xw)
+    B=np.array(B)
+    A=np.array(A)
+
+    info=np.array(info)
+    
+    return lab,X,Xw,B,A,info
+
+
+
+
 def makearrCS(flist,default_pick=0):
     #LC and BIP
     #,[lab],lcs,lcsw,asinds,asindsw,infogap,infogapw,starinfo
